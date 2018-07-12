43078176349cb3342ab91b7d4ab21b4c07442db6
Balakumaren <41141630+Balakumaren@users.noreply.github.com>
2018-07-12 15:00:49 +0530

    Create max.py

---
+++ max.py
@@ -0,0 +1,12 @@
+a=input()
+a=a.split(" ")
+a[0]=int(a[0])
+a[1]=int(a[1])
+a[2]=int(a[2])
+if(a[0]>a[1]):
+    if(a[0]>a[2]):
+        print(a[0])
+    else:
+        print(a[2])
+else:
+    print(a[1])
