dcda72edc2c459efd6acc9a6c005f854163b2d16
Balakumaren <41141630+Balakumaren@users.noreply.github.com>
2018-07-12 14:37:50 +0530

    Update file13.py

--- file13.py
+++ file13.py
@@ -1,5 +1,9 @@
 a=input()
-if(int(a)%2==0):
-    print("Even")
+b=int(a)
+if(b>0):
+    if(b%2==0):
+        print("Even")
+    else:
+        print("Odd")
 else:
-    print("Odd")
+    print("Invalid")
